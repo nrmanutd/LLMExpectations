165745aa29f4a8ceabeb77feaa44a128263a0fb6
nrmanutd <nrmanutd@gmail.com>
2026-07-31 16:53:16 +0300

    added mlcluster surveyer logic

--- runMultipleSurveyes.py
+++ runMultipleSurveyes.py
@@ -18,15 +18,15 @@ from SurveyLogic.surveyHelpers import createSurveyRunner
 
 logger = SimpleLogger()
 profilesFolder = Path('./data/Target profiles')
-resultsFolder = Path('data/SurveyResults/mlcluster_qwen36_no_inflation_no_politics_no_date_2016_2026_QS')
+resultsFolder = Path('data/SurveyResults/mlcluster_qwen36_official_inflation_avgbuyings_mrot_2016_2026_QS')
 surveyDates = pd.date_range(start='2016-04-01', end='2026-04-01', freq='QS', inclusive='both').tolist()
 
 #systemPromptBuilder, promptBuilder = createSimplePromptBuilder()
 systemPromptBuilder, promptBuilder = createCustomPromptBuilder(useEconomy=False, usePolitics=False, useStateInflation=True, useRegionalInflation=True)
 
 #surveyer = BothubSurveyer(modelToUse='deepseek-v4-pro', key=bothub_key, logger=logger)
-#surveyer = MLClusterSurveyer(modelToUse='Qwen/Qwen3.6-27B', key=mlcluster_key, logger=logger)
-surveyer = StubSurveyer()
+surveyer = MLClusterSurveyer(modelToUse='Qwen/Qwen3.6-27B', key=mlcluster_key, logger=logger)
+#surveyer = StubSurveyer()
 
 surveySerializer = SurveySerializer(resultsFolder)
 runner = createSurveyRunner(profilesFolder, systemPromptBuilder, promptBuilder, surveySerializer, surveyer, logger)
