482329dc6bcedd600c73500e8b610382abd004dc
nrmanutd <nrmanutd@gmail.com>
2026-08-13 15:07:11 +0300

    minor

--- SurveyLogic/PromptBuilders/profileBuildersHelpers.py
+++ SurveyLogic/PromptBuilders/profileBuildersHelpers.py
@@ -2,45 +2,16 @@ from Configuration import configuration
 from SHAPAnalysis.shapHelpers import getBitArray
 from SurveyLogic.PromptBuilders.BasePromptBuilder import BasePromptBuilder
 from SurveyLogic.PromptBuilders.CompositePromptBuilder import CompositePromptBuilder
-from SurveyLogic.PromptBuilders.ConstantPromptBuilder import ConstantPromptBuilder
 from SurveyLogic.PromptBuilders.ContextPromptBuilders.NewsPromptBuilder import NewsPromptBuilder
-from SurveyLogic.PromptBuilders.ContextPromptBuilders.StatePromptBuilders.RegionalInflationContextPromptBuilder import \
-    RegionalInflationContextPromptBuilder
-from SurveyLogic.PromptBuilders.ContextPromptBuilders.StatePromptBuilders.StateEconomyContextPromptBuilder import \
-    StateEconomyContextPromptBuilder
-from SurveyLogic.PromptBuilders.ContextPromptBuilders.StatePromptBuilders.StateInflationContextPromptBuilder import \
-    StateInflationContextPromptBuilder
-from SurveyLogic.PromptBuilders.MonthlyFromFilePromptBuilder import MonthlyFromFilePromptBuilder
 from SurveyLogic.PromptBuilders.ProfileSepcificPromptBuilders.CommonProfilePromptBuilder import \
     CommonProfilePromptBuilder
-from SurveyLogic.PromptBuilders.ProfileSepcificPromptBuilders.ExpensesProfilePromptBuilder import \
-    ExpensesProfilePromptBuilder
-from SurveyLogic.PromptBuilders.ProfileSepcificPromptBuilders.HouseholdProfilePromptBuilder import \
-    HouseholdProfilePromptBuilder
-from SurveyLogic.PromptBuilders.ProfileSepcificPromptBuilders.StateExpensesProfilePromptBuilder import \
-    StateExpensesProfilePromptBuilder
 from SurveyLogic.PromptBuilders.PromptBuilderFactory import PromptBuilderFactory
+from SurveyLogic.PromptBuilders.Prompts import prompts
 from SurveyLogic.PromptBuilders.StatisticsProviders.AverageExpensesProvider import AverageExpensesProvider
 from SurveyLogic.PromptBuilders.StatisticsProviders.ConvertingAverageExpensesProvider import \
     ConvertingAverageExpensesProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.BaseSingleMonthInflationProvider import \
-    BaseSingleMonthInflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.ConvertingInflationProvider import ConvertingInflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.DateRoundingSingleMonthInflationProvider import \
-    DateRoundingSingleMonthInflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.EMISSWebSingleMonthInflationProvider import \
-    EMISSWebSingleMonthInflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.InflationProvider import InflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.MultipleEMISSFilesInflationProvider import \
-    MultipleEMISSFilesInflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.MultipleWeeklyInflationProvider import \
-    MultipleWeeklyInflationProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.InflationProviderLogic.RosstatWeeklyInflationProvider import \
-    RosstatWeeklyInflationProvider
 from SurveyLogic.PromptBuilders.StatisticsProviders.MROTProvider import MROTProvider
-from SurveyLogic.PromptBuilders.StatisticsProviders.USDRUBRateProvider import USDRUBRateProvider
 from SurveyLogic.PromptBuilders.SystemPromptBuilder import SystemPromptBuilder
-from SurveyLogic.PromptBuilders.Prompts import prompts
 from SurveyLogic.PromptBuilders.TaskPromptBuilder import TaskPromptBuilder
 from experimentsConfiguration import ExperimentsConfiguration
 
@@ -58,7 +29,12 @@ def createSimplePromptBuilder() -> (BasePromptBuilder, BasePromptBuilder):
 def createNewsPromptBuilder() -> (BasePromptBuilder, BasePromptBuilder):
     builders = []
 
-    builders.append(CommonProfilePromptBuilder(prompts.respondentPrompt))
+    mrotProvider = MROTProvider(configuration.mrotStatisticsPath)
+    averageBuyingsProvider = AverageExpensesProvider(configuration.averageBuyingsDataPath)
+    averageBuyingsProvider = ConvertingAverageExpensesProvider(averageBuyingsProvider,
+                                                               configuration.rlmsToInflationRegionsPath)
+
+    builders.append(CommonProfilePromptBuilder(prompts.respondentPrompt, mrotProvider, averageBuyingsProvider))
     builders.append(NewsPromptBuilder())
     builders.append(TaskPromptBuilder(prompts.taskPrompt))
 
