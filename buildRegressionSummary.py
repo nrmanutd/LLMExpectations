fixed rmse calculation

buildRegressionSummary.py
@@ -4,9 +4,8 @@ from pathlib import Path
 
 import numpy as np
 
-from Configuration import visualizationConfiguration, configuration
+from Configuration import configuration
 from SurveyLogic.PromptBuilders.PromptBuilderFactory import PromptBuilderFactory
-from SurveyResultsAnalysis.RegressionAnalysis.CachingSurveyRegressionService import CachingSurveyRegressionService
 from SurveyResultsAnalysis.RegressionAnalysis.ForecastRobustness import ForecastRobustness
 from SurveyResultsAnalysis.RegressionAnalysis.Learning.AllVariablesProvider import AllVariablesProvider
 from SurveyResultsAnalysis.RegressionAnalysis.Learning.RegressionLearner import RegressionLearner
@@ -54,7 +53,8 @@ modellingResults = [
 ]
 
 featuresDescriptionPath = Path('data/LLMSurveys_Configurations.xlsx')
-OOSStartPoints = 30
+
+OOSStartPointsArray = [30, 100]
 
 nStepsAhead = [1, 2, 3, 4, 5, 6]
 useDelta = [False]
@@ -62,15 +62,13 @@ useOOS = [True]
 useExpandingOOS = [True]
 useDummy = [True]
 variables = ['IE', 'CPI']
-useXGBoost = [True]
+useXGBoost = [False, True]
 
 includeDatesFilter = []
-excludeDatesFilter = [('До 01.01.2022', '2022-01-01', '2027-02-01'), ('Без начала СВО 23.02.22-01.06.22', '2022-02-23', '2022-06-01'), ('Весь период', '2030-01-01', '2030-01-02'), ('После 01.01.2022', '2000-01-01', '2022-01-01')]
-#excludeDatesFilter = [('Весь период', '2030-01-01', '2030-01-02')]
+#excludeDatesFilter = [('До 01.01.2022', '2022-01-01', '2027-02-01'), ('Без начала СВО 23.02.22-01.06.22', '2022-02-23', '2022-06-01'), ('Весь период', '2030-01-01', '2030-01-02'), ('После 01.01.2022', '2000-01-01', '2022-01-01')]
+excludeDatesFilter = [('Весь период', '2030-01-01', '2030-01-02')]
 
 surveyResults = loadSurveyResults(rootFolder, modellingResults)
-
-
 directEstimations = load_from_official_statistics(configuration.inflationExpectations, 1)
 
 inflationProvider = PromptBuilderFactory.createInflationProvider()
@@ -78,104 +76,109 @@ usdrubRateProvider = PromptBuilderFactory.createCurrencyProvider()
 keyRateProvider = PromptBuilderFactory.createKeyRateProvider()
 inflationExpectationsProvider = PromptBuilderFactory.createInflationExpectationsProvider()
 
-green_max_flags = [True, False, True, True, True, False, True]
+green_max_flags = [True, False, False, True, True, True, False, True]
 tStart = time.time()
 
 row_names = [x[1] for x in modellingResults]
 col_names = [f'{f'{x - 1} мес + ' if x > 1 else ''}1 нед' for x in nStepsAhead]
 
 featuresMatrix = getFeaturesDescriptions(featuresDescriptionPath, row_names)
-
 for i_learner in range(len(useXGBoost)):
-    for i_excludeDatesFilter in range(len(excludeDatesFilter)):
-        print(f'[{time.time() - tStart:.2f}s] Датасет: {excludeDatesFilter[i_excludeDatesFilter]}')
-        for var in variables:
-            for i_useDelta in range(len(useDelta)):
-                for i_useOOS in range(len(useOOS)):
-                    for i_useExpandingOOS in range(len(useExpandingOOS)):
-                        for i_useDummy in range(len(useDummy)):
-                            isDelta = useDelta[i_useDelta]
-                            isOOS = useOOS[i_useOOS]
-                            isExpandingOOS = useExpandingOOS[i_useExpandingOOS]
-                            excludeDate = excludeDatesFilter[i_excludeDatesFilter]
-                            isDummy = useDummy[i_useDummy]
-                            isXGBoost = useXGBoost[i_learner]
+    for i_oosstartingpoints in range(len(OOSStartPointsArray)):
+        for i_excludeDatesFilter in range(len(excludeDatesFilter)):
+            print(f'[{time.time() - tStart:.2f}s] Датасет: {excludeDatesFilter[i_excludeDatesFilter]}')
+            for var in variables:
+                for i_useDelta in range(len(useDelta)):
+                    for i_useOOS in range(len(useOOS)):
+                        for i_useExpandingOOS in range(len(useExpandingOOS)):
+                            for i_useDummy in range(len(useDummy)):
+                                isDelta = useDelta[i_useDelta]
+                                isOOS = useOOS[i_useOOS]
+                                isExpandingOOS = useExpandingOOS[i_useExpandingOOS]
+                                excludeDate = excludeDatesFilter[i_excludeDatesFilter]
+                                isDummy = useDummy[i_useDummy]
+                                isXGBoost = useXGBoost[i_learner]
+                                OOSStartPoints = OOSStartPointsArray[i_oosstartingpoints]
 
-                            if isXGBoost:
-                                variablesProvider = AllVariablesProvider(isDelta, featuresMatrix)
-                                m = 'XGBoost'
-                                learner = XGBoostLearner(isDummy)
-                            else:
-                                variablesProvider = RegressionVariablesProvider(isDelta)
-                                m = 'AR(1)'
-                                learner = RegressionLearner(isDummy)
+                                if isXGBoost:
+                                    variablesProvider = AllVariablesProvider(isDelta, featuresMatrix)
+                                    m = 'XGBoost'
+                                    learner = XGBoostLearner(isDummy)
+                                else:
+                                    variablesProvider = RegressionVariablesProvider(isDelta)
+                                    m = 'AR(1)'
+                                    learner = RegressionLearner(isDummy)
 
-                            datesToExclude = (np.datetime64(excludeDate[1]), np.datetime64(excludeDate[2]))
+                                datesToExclude = (np.datetime64(excludeDate[1]), np.datetime64(excludeDate[2]))
 
-                            headers = [f'Relative RMSE Gain {m} + LLM vs {m}',
-                                       f'Clark-West test {m} + LLM vs {m}', 'Share of points LLM is better',
-                                       'Median LOO', 'MIN LOO', 'Share of best point in total gain', 'Adj. R² gain']
+                                headers = [f'Relative RMSE Gain {m} + LLM vs {m}', f'LLM + {m} RMSE',
+                                           f'Clark-West test {m} + LLM vs {m}', 'Share of points LLM is better',
+                                           'Median LOO', 'MIN LOO', 'Share of best point in total gain', 'Adj. R² gain']
 
-                            relativeRMSEGain = np.zeros((len(modellingResults), len(nStepsAhead)))
-                            pValueCWTest = np.zeros((len(modellingResults), len(nStepsAhead)))
-                            shareOfLLMBetter = np.zeros((len(modellingResults), len(nStepsAhead)))
-                            loom = np.zeros((len(modellingResults), len(nStepsAhead)))
-                            minloo = np.zeros((len(modellingResults), len(nStepsAhead)))
-                            shareOfBestPoint = np.zeros((len(modellingResults), len(nStepsAhead)))
-                            adjRSquared = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                relativeRMSEGain = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                llmRMSE = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                pValueCWTest = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                shareOfLLMBetter = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                loom = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                minloo = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                shareOfBestPoint = np.zeros((len(modellingResults), len(nStepsAhead)))
+                                adjRSquared = np.zeros((len(modellingResults), len(nStepsAhead)))
 
-                            matrices = [relativeRMSEGain, pValueCWTest, shareOfLLMBetter, loom, minloo, shareOfBestPoint, adjRSquared]
+                                matrices = [relativeRMSEGain, llmRMSE, pValueCWTest, shareOfLLMBetter, loom, minloo, shareOfBestPoint, adjRSquared]
 
-                            dataSetCreator = StandardDatasetCreator(directEstimations, inflationProvider,
-                                                                    usdrubRateProvider,
-                                                                    keyRateProvider, inflationExpectationsProvider, var,
-                                                                    isDelta, isDummy,
-                                                                    datesToExclude)
+                                dataSetCreator = StandardDatasetCreator(directEstimations, inflationProvider,
+                                                                        usdrubRateProvider,
+                                                                        keyRateProvider, inflationExpectationsProvider, var,
+                                                                        isDelta, isDummy,
+                                                                        datesToExclude)
 
-                            surveyRegressionService = SurveyRegressionService(dataSetCreator, learner)
-                            surveyRegressionService = CachingSurveyRegressionService(surveyRegressionService)
+                                surveyRegressionService = SurveyRegressionService(dataSetCreator, learner)
+                                #surveyRegressionService = CachingSurveyRegressionService(surveyRegressionService)
 
-                            for i in range(len(modellingResults)):
-                                print(f'[{time.time() - tStart:.2f}s] Model: {modellingResults[i][1]}')
-                                modelKey = modellingResults[i][1]
+                                for i in range(len(modellingResults)):
+                                    print(f'[{time.time() - tStart:.2f}s] Model #{i + 1} of {len(modellingResults)}: {modellingResults[i][1]}')
+                                    modelKey = modellingResults[i][1]
 
-                                survey = surveyResults[modelKey]
-                                baseVariables = variablesProvider.getBaseVariables(modelKey)
+                                    survey = surveyResults[modelKey]
+                                    baseVariables = variablesProvider.getBaseVariables(modelKey)
 
-                                targetModelVariables = (['X3'] + baseVariables, 'Y')
-                                baseModelVariables = (baseVariables, 'Y')
+                                    targetModelVariables = (['X3'] + baseVariables, 'Y')
+                                    baseModelVariables = (baseVariables, 'Y')
 
-                                if isDummy:
-                                    targetModelVariables[0].append('X7')
-                                    baseModelVariables[0].append('X7')
+                                    if isDummy:
+                                        targetModelVariables[0].append('X7')
+                                        baseModelVariables[0].append('X7')
 
-                                for j in range(len(nStepsAhead)):
-                                    print(f'[{time.time() - tStart:.2f}s] nStepsAhead = {nStepsAhead[j]}...')
-                                    curNMonth=nStepsAhead[j]
+                                    for j in range(len(nStepsAhead)):
+                                        print(f'[{time.time() - tStart:.2f}s] nStepsAhead = {nStepsAhead[j]}...')
+                                        curNMonth=nStepsAhead[j]
 
-                                    ty, tr, tm, tdates = surveyRegressionService.fitWithConfig(survey, targetModelVariables, isOOS, isExpandingOOS, start_n=OOSStartPoints, nMonth=curNMonth)
-                                    by, br, bm, bdates = surveyRegressionService.fitWithConfig(survey, baseModelVariables, isOOS, isExpandingOOS, start_n=OOSStartPoints, nMonth=curNMonth)
+                                        ty, tr, tm, tdates = surveyRegressionService.fitWithConfig(survey, targetModelVariables, isOOS, isExpandingOOS, start_n=OOSStartPoints, nMonth=curNMonth)
+                                        by, br, bm, bdates = surveyRegressionService.fitWithConfig(survey, baseModelVariables, isOOS, isExpandingOOS, start_n=OOSStartPoints, nMonth=curNMonth)
 
-                                    e_llm = ty - tr
-                                    e_base = by - br
+                                        e_llm = ty - tr
+                                        e_base = by - br
 
-                                    fr = ForecastRobustness(e_llm, e_base)
-                                    #fr.print_robustness_report(block_size=4, n_boot=10_000, hac_lags=3)
-                                    loo_results, loo = fr.leave_one_out_gain()
-                                    cw = fr.clark_west_test()
+                                        llm_rmse = np.sqrt(np.mean(e_llm**2))
 
-                                    sse = loo['full_gain_percent']
-                                    relativeRMSEGainValue = 1 - math.sqrt(1 - sse/100)
+                                        fr = ForecastRobustness(e_llm, e_base)
+                                        #fr.print_robustness_report(block_size=4, n_boot=10_000, hac_lags=3)
+                                        loo_results, loo = fr.leave_one_out_gain()
+                                        cw = fr.clark_west_test()
 
-                                    relativeRMSEGain[i, j] = relativeRMSEGainValue
-                                    minloo[i, j] = loo['loo_min_percent'] / 100
-                                    loom[i, j] = loo['loo_median']
-                                    shareOfLLMBetter[i, j] = loo['share_positive']
-                                    shareOfBestPoint[i, j] = (loo["full_gain"] - loo["loo_min"]) / loo["full_gain"]
-                                    pValueCWTest[i, j] = cw["p_value_one_sided"]
+                                        sse = loo['full_gain_percent']
+                                        relativeRMSEGainValue = 1 - math.sqrt(1 - sse/100)
 
-                                    if not isOOS:
-                                        adjRSquared[i, j] = tm.rsquared_adj - bm.rsquared_adj
+                                        relativeRMSEGain[i, j] = relativeRMSEGainValue
+                                        llmRMSE[i, j] = llm_rmse
+                                        minloo[i, j] = loo['loo_min_percent'] / 100
+                                        loom[i, j] = loo['loo_median']
+                                        shareOfLLMBetter[i, j] = loo['share_positive']
+                                        shareOfBestPoint[i, j] = (loo["full_gain"] - loo["loo_min"]) / loo["full_gain"]
+                                        pValueCWTest[i, j] = cw["p_value_one_sided"]
 
-                            filePrefix = f'{var}_{excludeDate[0]}_{'delta' if isDelta else 'level'}_{'oos' if isOOS else 'in sample'}_{'expanding' if isExpandingOOS else 'fixed split'}_start points={OOSStartPoints}_{'dummy' if isDummy else 'no_dummy'}_{'xgb' if isXGBoost else 'r'}_({len(modellingResults)})'
-                            saveMatricesToExcel(matrices, headers, filePrefix, row_names, col_names, green_max_flags, featuresMatrix)
+                                        if not isOOS:
+                                            adjRSquared[i, j] = tm.rsquared_adj - bm.rsquared_adj
+
+                                filePrefix = f'{var}_{excludeDate[0]}_{'delta' if isDelta else 'level'}_{'oos' if isOOS else 'in sample'}_{'expanding' if isExpandingOOS else 'fixed split'}_start points={OOSStartPoints}_{'dummy' if isDummy else 'no_dummy'}_{'xgb' if isXGBoost else 'r'}_({len(modellingResults)})'
+                                saveMatricesToExcel(matrices, headers, filePrefix, row_names, col_names, green_max_flags, featuresMatrix)